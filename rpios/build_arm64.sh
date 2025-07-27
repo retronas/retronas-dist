cac83012e8f4e43eba26e1f53d470b285e015401
sairuk <sairuk@gmail.com>
2025-07-27 11:25:19 +1000

    more tweaks

--- rpios/build_arm64.sh
+++ rpios/build_arm64.sh
@@ -2,11 +2,7 @@
 
 . image.sh
 
-function download_arm64 {
-  download "https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2025-05-13/2025-05-13-raspios-bookworm-arm64-lite.img.xz"
-}
-
 check arm64
-download_arm64
+download "https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2025-05-13/2025-05-13-raspios-bookworm-arm64-lite.img.xz" arm64
 uncompress arm64
 ./repack.sh
--- rpios/build_armhf.sh
+++ rpios/build_armhf.sh
@@ -2,11 +2,7 @@
 
 . image.sh
 
-function download_armhf {
-  download "https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2025-05-13/2025-05-13-raspios-bookworm-armhf-lite.img.xz"
-}
-
 check armhf
-download_armhf
+download "https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2025-05-13/2025-05-13-raspios-bookworm-armhf-lite.img.xz" armhf
 uncompress armhf
 ./repack.sh
